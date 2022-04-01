4660ddc6214b606225989ec0c3346bbe99c33c37
Matt Ross <matt@tronik.co.uk>
2022-04-01 11:33:42 +0100

    Add UI via dotnet new isui

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace IdentityServer;
 
@@ -7,7 +8,8 @@ public static class Config
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
         { 
-            new IdentityResources.OpenId()
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
@@ -34,6 +36,26 @@ public static class Config
 
                     // scopes that client has access to
                     AllowedScopes = { "apiLS" }
+                },
+                // interactive ASP.NET Core Web App
+                new Client
+                {
+                    ClientId = "webLS",
+                    ClientSecrets = { new Secret("topsecret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+            
+                    // where to redirect to after login
+                    RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
 }
