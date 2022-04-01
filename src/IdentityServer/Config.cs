8e6001ba1c92eea386f988b0a68a57d83d28d683
Matt Ross <matt@tronik.co.uk>
2022-04-01 11:28:57 +0100

    Add Client

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -18,5 +18,22 @@ public static class Config
 
     public static IEnumerable<Client> Clients =>
         new Client[] 
-            { };
+            {
+                new Client
+                {
+                    ClientId = "leedssharpclient",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("topsecret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "apiLS" }
+                }
+            };
 }
